3ffab61a81b99b81e9debb30c019dd0f8fc11393
HilmiKamil <117153086+HilmiKamil@users.noreply.github.com>
2024-11-06 10:02:35 +0700

    Update api.php

--- Rest-API/routes/api.php
+++ Rest-API/routes/api.php
@@ -3,10 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\StudentController;
->>>>>>> 4af06d4 (Rest API)
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -23,12 +20,9 @@ Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
 # method delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
-<<<<<<< HEAD
-=======
 
 Route::get('/student', [StudentController::class, 'index']);
 Route::post('/student', [StudentController::class, 'store']);
 Route::get('/student/{id}', [StudentController::class, 'show']);
 Route::put('/student/{id}', [StudentController::class, 'update']);
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
->>>>>>> 4af06d4 (Rest API)
