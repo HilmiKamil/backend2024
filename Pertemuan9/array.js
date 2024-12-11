cfb11465d7a0ee8ba2a1cae96ffe894958e3892b
HilmiKamil <hilmihelios@gmail.com>
2024-12-12 06:47:34 +0700

    Praktikum 9

---
+++ Pertemuan9/array.js
@@ -0,0 +1,10 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// mengakses array
+console.log(fruits[0], fruits[1]);
+
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+console.log(fruits.nama);
